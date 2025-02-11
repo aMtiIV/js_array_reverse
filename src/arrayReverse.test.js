fe71d777e501b1932d799de66027dfc65e26c8df
Mateusz Oślizło <amti4osl@gmail.com>
2025-02-11 12:59:47 +0100

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,41 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['Mate', 'Academy'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`should reverse single string`, () => {
+    expect(arrayReverse(['Mate'])).toEqual(['etaM']);
+  });
+
+  it(`should reverse With numbers`, () => {
+    expect(arrayReverse(['1Mate0'])).toEqual(['0etaM1']);
+  });
+
+  it(`should keep empty string when reversing the other`, () => {
+    expect(arrayReverse(['Mate', ''])).toEqual(['etaM', '']);
+  });
+
+  it(`should return multiple empty strings`, () => {
+    expect(arrayReverse(['', ''])).toEqual(['', '']);
+  });
+
+  it(`should reverse multiple strings`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should reverse all four strings`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
+  it(`should reverse special chars`, () => {
+    expect(arrayReverse(['@#', '$%^', '&*()', '-_=+!?']))
+      .toEqual(['?!', '+=_', '-)(*', '&^%$#@']);
+  });
 });
